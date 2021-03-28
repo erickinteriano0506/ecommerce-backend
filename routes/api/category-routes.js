288644ca678c2a834461c6f8c8dcedd93f00953b
E Alexander <erick.interiano05@gmail.com>
2021-03-28 16:04:15 -0700

    Made route to create new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,6 +53,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCatdata => 
+    res.json(dbCatdata))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
