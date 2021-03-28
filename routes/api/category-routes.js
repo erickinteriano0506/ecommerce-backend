97235b5c2cf910c2afa6e1cc5e45e739c6737b7b
E Alexander <erick.interiano05@gmail.com>
2021-03-28 15:53:00 -0700

    Created get route for all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,29 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then(dbCatdata => {
+    if(!dbCatdata) {
+      res.status(404).json({message: 'No Categories Found'});
+      return;
+    }
+    res.json(dbCatdata);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
